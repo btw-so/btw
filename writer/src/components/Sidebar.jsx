d07ec4e90f6486fdd9bc747c6a032d086295c1a0
siddhartha gunti <siddhartha.gunti191@gmail.com>
2023-03-22 17:15:11 +0800

    creating collapsed navigation + all notes/published notes/working notes theme

--- writer/src/components/Sidebar.jsx
+++ writer/src/components/Sidebar.jsx
@@ -7,6 +7,7 @@ import useInterval from "beautiful-react-hooks/useInterval";
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { STATUS } from "../literals";
+import useLocalStorage from "../hooks/useLocalStorage";
 import {
   getNotes,
   createNewNote,
@@ -19,6 +20,19 @@ function Sidebar(props) {
   const notesState = useAppSelector(selectNotes);
   const dispatch = useDispatch();
   const navigate = useNavigate();
+
+  const [expandAllNotes, setExpandAllNotes] = useLocalStorage(
+    "expandAllNotes",
+    true
+  );
+  const [expandPublishedNotes, setExpandPublishedNotes] = useLocalStorage(
+    "expandPublishedNotes",
+    true
+  );
+  const [expandUnpublishedNotes, setExpandUnpublishedNotes] = useLocalStorage(
+    "expandUnpublishedNotes",
+    true
+  );
 
   useInterval(() => {
     if (token && notesState.notesList.status !== STATUS.RUNNING) {
@@ -39,6 +53,31 @@ function Sidebar(props) {
       );
     }
   }, [token]);
+
+  var noteLists = [
+    {
+      title: "All notes",
+      notes: notesState.notesList.data,
+      expanded: expandAllNotes,
+      toggle: () => setExpandAllNotes(!expandAllNotes),
+    },
+    {
+      title: "Published notes",
+      notes: notesState.notesList.data.filter(
+        (id) => notesState.notesMap[id].publish
+      ),
+      expanded: expandPublishedNotes,
+      toggle: () => setExpandPublishedNotes(!expandPublishedNotes),
+    },
+    {
+      title: "Working notes",
+      notes: notesState.notesList.data.filter(
+        (id) => !notesState.notesMap[id].publish
+      ),
+      expanded: expandUnpublishedNotes,
+      toggle: () => setExpandUnpublishedNotes(!expandUnpublishedNotes),
+    },
+  ];
 
   if (token) {
     return (
@@ -62,53 +101,73 @@ function Sidebar(props) {
           </button>
         </div>
         <div className="flex-grow overflow-y-auto">
-          <div>
-            <div className="flex items-center">
-              <i className={`ri-1x ri-arrow-right-down-line`}></i>
-              <span className="font-extrabold">All notes</span>
-            </div>
-            <ul className="text-black pl-4">
-              {notesState.notesList.data.map((id) => {
-                let note = notesState.notesMap[id];
-                let d = new Date(note.updated_at || note.created_at);
-                // convert do to MMM DD, YYYY
-                let date = d.toLocaleDateString("en-US", {
-                  month: "short",
-                  day: "numeric",
-                  year: "numeric",
-                });
-                return (
-                  <li
-                    className={`mb-2 py-2 px-3 text-gray-500 cursor-pointer hover:bg-gray-200 pl-2 ${
-                      notesState.selectedNoteId === id
-                        ? "border-l-2 border-solid border-blue-500"
-                        : ""
+          {noteLists.map((noteList) => {
+            return (
+              <div key={noteList.title} className="mb-4">
+                <div
+                  className="flex items-center cursor-pointer"
+                  onClick={() => {
+                    noteList.toggle();
+                  }}
+                >
+                  <i
+                    className={`ri-1x ri-arrow-down-s-line ${
+                      noteList.expanded ? "" : "hidden"
                     }`}
-                    key={id}
-                    onClick={() => {
-                      dispatch(
-                        selectNote({
-                          id,
-                        })
+                  ></i>
+                  <i
+                    className={`ri-1x ri-arrow-right-s-line ${
+                      noteList.expanded ? "hidden" : ""
+                    }`}
+                  ></i>
+                  <span className="font-extrabold">{noteList.title}</span>
+                </div>
+                {noteList.expanded ? (
+                  <ul className="text-black pl-4">
+                    {noteList.notes.map((id) => {
+                      let note = notesState.notesMap[id];
+                      let d = new Date(note.updated_at || note.created_at);
+                      // convert do to MMM DD, YYYY
+                      let date = d.toLocaleDateString("en-US", {
+                        month: "short",
+                        day: "numeric",
+                        year: "numeric",
+                      });
+                      return (
+                        <li
+                          className={`mb-2 py-2 px-3 text-gray-500 cursor-pointer hover:bg-gray-200 pl-2 ${
+                            notesState.selectedNoteId === id
+                              ? "border-l-2 border-solid border-blue-500"
+                              : ""
+                          }`}
+                          key={id}
+                          onClick={() => {
+                            dispatch(
+                              selectNote({
+                                id,
+                              })
+                            );
+                            navigate("/dash");
+                          }}
+                        >
+                          <span
+                            className={`text-sm block ${
+                              notesState.selectedNoteId === id
+                                ? "font-bold text-black"
+                                : "text-black"
+                            }`}
+                          >
+                            {note.title || "New Note"}
+                          </span>
+                          <span className="text-xs block">{date}</span>
+                        </li>
                       );
-                      navigate("/dash");
-                    }}
-                  >
-                    <span
-                      className={`text-sm block ${
-                        notesState.selectedNoteId === id
-                          ? "font-bold text-black"
-                          : "text-black"
-                      }`}
-                    >
-                      {note.title || "New Note"}
-                    </span>
-                    <span className="text-xs block">{date}</span>
-                  </li>
-                );
-              })}
-            </ul>
-          </div>
+                    })}
+                  </ul>
+                ) : null}
+              </div>
+            );
+          })}
         </div>
         <div className="w-full border-t-2 border-gray-200 sidebar-toolkit">
           <button
