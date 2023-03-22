afd3dda162529226b8598469d97d160a5a26524e
siddhartha gunti <siddhartha.gunti191@gmail.com>
2023-03-22 17:28:58 +0800

    nit ux change

--- writer/src/components/Sidebar.jsx
+++ writer/src/components/Sidebar.jsx
@@ -27,11 +27,11 @@ function Sidebar(props) {
   );
   const [expandPublishedNotes, setExpandPublishedNotes] = useLocalStorage(
     "expandPublishedNotes",
-    true
+    false
   );
   const [expandUnpublishedNotes, setExpandUnpublishedNotes] = useLocalStorage(
     "expandUnpublishedNotes",
-    true
+    false
   );
 
   useInterval(() => {
@@ -101,73 +101,75 @@ function Sidebar(props) {
           </button>
         </div>
         <div className="flex-grow overflow-y-auto">
-          {noteLists.map((noteList) => {
-            return (
-              <div key={noteList.title} className="mb-4">
-                <div
-                  className="flex items-center cursor-pointer"
-                  onClick={() => {
-                    noteList.toggle();
-                  }}
-                >
-                  <i
-                    className={`ri-1x ri-arrow-down-s-line ${
-                      noteList.expanded ? "" : "hidden"
-                    }`}
-                  ></i>
-                  <i
-                    className={`ri-1x ri-arrow-right-s-line ${
-                      noteList.expanded ? "hidden" : ""
-                    }`}
-                  ></i>
-                  <span className="font-extrabold">{noteList.title}</span>
-                </div>
-                {noteList.expanded ? (
-                  <ul className="text-black pl-4">
-                    {noteList.notes.map((id) => {
-                      let note = notesState.notesMap[id];
-                      let d = new Date(note.updated_at || note.created_at);
-                      // convert do to MMM DD, YYYY
-                      let date = d.toLocaleDateString("en-US", {
-                        month: "short",
-                        day: "numeric",
-                        year: "numeric",
-                      });
-                      return (
-                        <li
-                          className={`mb-2 py-2 px-3 text-gray-500 cursor-pointer hover:bg-gray-200 pl-2 ${
-                            notesState.selectedNoteId === id
-                              ? "border-l-2 border-solid border-blue-500"
-                              : ""
-                          }`}
-                          key={id}
-                          onClick={() => {
-                            dispatch(
-                              selectNote({
-                                id,
-                              })
-                            );
-                            navigate("/dash");
-                          }}
-                        >
-                          <span
-                            className={`text-sm block ${
+          {noteLists
+            .filter((x) => x.notes && x.notes.length > 0)
+            .map((noteList) => {
+              return (
+                <div key={noteList.title} className="mb-4">
+                  <div
+                    className="flex items-center cursor-pointer"
+                    onClick={() => {
+                      noteList.toggle();
+                    }}
+                  >
+                    <i
+                      className={`ri-1x ri-arrow-down-s-line ${
+                        noteList.expanded ? "" : "hidden"
+                      }`}
+                    ></i>
+                    <i
+                      className={`ri-1x ri-arrow-right-s-line ${
+                        noteList.expanded ? "hidden" : ""
+                      }`}
+                    ></i>
+                    <span className="font-extrabold">{noteList.title}</span>
+                  </div>
+                  {noteList.expanded ? (
+                    <ul className="text-black pl-4">
+                      {noteList.notes.map((id) => {
+                        let note = notesState.notesMap[id];
+                        let d = new Date(note.updated_at || note.created_at);
+                        // convert do to MMM DD, YYYY
+                        let date = d.toLocaleDateString("en-US", {
+                          month: "short",
+                          day: "numeric",
+                          year: "numeric",
+                        });
+                        return (
+                          <li
+                            className={`mb-2 py-2 px-3 text-gray-500 cursor-pointer hover:bg-gray-200 pl-2 ${
                               notesState.selectedNoteId === id
-                                ? "font-bold text-black"
-                                : "text-black"
+                                ? "border-l-2 border-solid border-blue-500"
+                                : ""
                             }`}
+                            key={id}
+                            onClick={() => {
+                              dispatch(
+                                selectNote({
+                                  id,
+                                })
+                              );
+                              navigate("/dash");
+                            }}
                           >
-                            {note.title || "New Note"}
-                          </span>
-                          <span className="text-xs block">{date}</span>
-                        </li>
-                      );
-                    })}
-                  </ul>
-                ) : null}
-              </div>
-            );
-          })}
+                            <span
+                              className={`text-sm block ${
+                                notesState.selectedNoteId === id
+                                  ? "font-bold text-black"
+                                  : "text-black"
+                              }`}
+                            >
+                              {note.title || "New Note"}
+                            </span>
+                            <span className="text-xs block">{date}</span>
+                          </li>
+                        );
+                      })}
+                    </ul>
+                  ) : null}
+                </div>
+              );
+            })}
         </div>
         <div className="w-full border-t-2 border-gray-200 sidebar-toolkit">
           <button
