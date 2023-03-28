f8c3d73e38e976e3ecf699157f24256afcd8ecae
siddhartha gunti <siddhartha.gunti191@gmail.com>
2023-03-28 16:51:06 +0800

    Adding v1 readme file + license + sample db setup

---
+++ README.md
@@ -0,0 +1,58 @@
+# BTW (By The Way)
+
+![Logo](logo.png)
+
+BTW (By The Way) is an open-source project that aims to [provide a brief description of the project's purpose or functionality]. This project is designed to be easy to use, highly customizable, and scalable.
+
+The cloud hosted version of this project is at [https://app.btw.so](https://app.btw.so)
+
+Sample of a blog that is published using btw:
+
+-   [https://www.siddg.com](https://www.siddg.com)
+
+## Table of Contents
+
+-   [Getting Started](#getting-started)
+    -   [Prerequisites](#prerequisites)
+    -   [Installation](#installation)
+-   [Usage](#usage)
+    -   [Basic Example](#basic-example)
+    -   [Advanced Usage](#advanced-usage)
+-   [Documentation](#documentation)
+-   [Contributing](#contributing)
+-   [License](#license)
+-   [Acknowledgments](#acknowledgments)
+
+## Getting Started
+
+These instructions will help you get a copy of the project up and running on your local machine for development and testing purposes.
+
+### Prerequisites
+
+-   Install latest Docker, Docker-compose in your system
+-   Working postgres instance
+-   SMTP credentials (Ex: from mailgun)
+
+### Installation
+
+Provide step-by-step instructions on how to set up the project on a local machine:
+
+1. Setup your database using btw.sql file
+2. Replace the variables in deploy/docker-compose.dev.yml
+
+-   TASKS_DATABASE_URL: Set the connection url to your PG DB
+-   SMTP_HOST, SMTP_PORT, SMTP_USER, SMTP_PASS, SMTP_FROM: SMTP access creds (Used to email OTPs for login)
+-   S3_ENDPOINT, REACT_APP_S3_ENDPOINT, S3_BUCKET, S3_KEY, S3_SECRET: (OPTIONAL) Set this if you need image uploads to work in editor
+-   ADMIN_EMAIL: Your email address
+-   SECRET: Unique secret
+
+### Running
+
+Execute following commands from the deploy folder:
+
+`docker-compose -f docker-compose.dev.yml up`
+
+Visit
+
+-   localhost:9000 to login and start writing
+-   If you set your slug and publish any articles, you can visit localhost:9222?domain=<slug>
---
+++ btw.sql
@@ -0,0 +1,89 @@
+-- -------------------------------------------------------------
+-- Database: btw
+-- Generation Time: 2023-03-28 16:22:10.3490
+-- -------------------------------------------------------------
+
+CREATE SCHEMA btw;
+
+DROP TABLE IF EXISTS "btw"."custom_domains";
+
+-- Sequence and defined type
+CREATE SEQUENCE IF NOT EXISTS btw.custom_domains_id_seq;
+
+-- Table Definition
+CREATE TABLE "btw"."custom_domains" (
+    "id" int4 NOT NULL DEFAULT nextval('btw.custom_domains_id_seq'::regclass),
+    "domain" text NOT NULL,
+    "user_id" int4 NOT NULL,
+    PRIMARY KEY ("domain","user_id")
+);
+
+DROP TABLE IF EXISTS "btw"."login_token";
+
+-- Sequence and defined type
+CREATE SEQUENCE IF NOT EXISTS btw.login_token_id_seq;
+
+-- Table Definition
+CREATE TABLE "btw"."login_token" (
+    "id" int4 NOT NULL DEFAULT nextval('btw.login_token_id_seq'::regclass),
+    "uuid" uuid NOT NULL,
+    "user_id" int4 NOT NULL,
+    "created_at" timestamptz NOT NULL,
+    "ip_address" text NOT NULL,
+    "fingerprint" text NOT NULL,
+    PRIMARY KEY ("uuid")
+);
+
+DROP TABLE IF EXISTS "btw"."notes";
+
+-- Table Definition
+CREATE TABLE "btw"."notes" (
+    "user_id" int4 NOT NULL,
+    "created_at" timestamptz NOT NULL,
+    "updated_at" timestamptz NOT NULL,
+    "json" json,
+    "html" text,
+    "title" text,
+    "id" uuid NOT NULL,
+    "ydoc" bytea,
+    "tags" text,
+    "published_at" timestamptz,
+    "slug" text,
+    "publish" bool,
+    "archive" bool,
+    "delete" bool,
+    "deleted_at" timestamptz,
+    PRIMARY KEY ("id","user_id")
+);
+
+DROP TABLE IF EXISTS "btw"."otp";
+
+-- Sequence and defined type
+CREATE SEQUENCE IF NOT EXISTS btw.otp_id_seq;
+
+-- Table Definition
+CREATE TABLE "btw"."otp" (
+    "id" int4 NOT NULL DEFAULT nextval('btw.otp_id_seq'::regclass),
+    "created_at" timestamptz NOT NULL,
+    "email" text NOT NULL,
+    "processed_email" text NOT NULL,
+    "otp" text NOT NULL,
+    PRIMARY KEY ("id")
+);
+
+DROP TABLE IF EXISTS "btw"."users";
+
+-- Sequence and defined type
+CREATE SEQUENCE IF NOT EXISTS btw.user_id_seq;
+
+-- Table Definition
+CREATE TABLE "btw"."users" (
+    "id" int4 NOT NULL DEFAULT nextval('btw.user_id_seq'::regclass),
+    "email" text NOT NULL,
+    "processed_email" text NOT NULL,
+    "name" text,
+    "slug" text,
+    "created_at" timestamptz NOT NULL,
+    PRIMARY KEY ("processed_email")
+);
+
