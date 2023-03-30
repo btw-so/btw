f6836675ecec64a721c25218e0aa1ce092725105
siddhartha gunti <siddhartha.gunti191@gmail.com>
2023-03-30 16:48:07 +0800

    cleaning up the HTML embed component so that its editable in place. replaces popup. allows multiple edits easily. sensible ux

--- writer/src/components/TipTapEmbed.js
+++ writer/src/components/TipTapEmbed.js
@@ -1,23 +1,159 @@
 import { Node, mergeAttributes } from "@tiptap/core";
+import {
+  NodeViewWrapper,
+  NodeViewContent,
+  ReactNodeViewRenderer,
+} from "@tiptap/react";
+import React, { useState, useRef, useEffect } from "react";
+import { createPortal } from "react-dom";
+
+const BtwEmbedNodeView = ({ node, updateAttributes, editor }) => {
+  const [isEditing, setEditing] = useState(node.attrs.code ? false : true);
+  const [blurRemoval, setBlurRemoval] = useState(false);
+  const textareaRef = useRef(null);
+
+  const setIsEditing = (val) => {
+    if (!val) {
+      // if there is no content in the textarea, delete the node
+      if (textareaRef.current && textareaRef.current.value === "") {
+        editor.commands.deleteSelection();
+        return;
+      }
+    }
+    setEditing(val);
+  };
+
+  // whenever editing changes to true, set focus on textarea
+  useEffect(() => {
+    if (isEditing) {
+      // set focus on textarea's ending
+      textareaRef.current.focus();
+      textareaRef.current.selectionStart = textareaRef.current.value.length;
+    }
+  }, [isEditing]);
+
+  useEffect(() => {
+    if (!node.attrs.code) {
+      setIsEditing(true);
+      setTimeout(() => {
+        textareaRef.current.focus();
+        setBlurRemoval(true);
+      }, 200);
+    } else {
+      setBlurRemoval(true);
+    }
+  }, []);
+
+  const getIframe = () => {
+    let html = (node.attrs.code || "").replace(/&quot;/g, '"');
+    const iframeId = "iframe-" + Math.random().toString(36).substr(2, 16);
+
+    const jsCode = `
+  <script>
+function sendHeightToParent() {
+    var height = document.body.scrollHeight;
+    var origin = window.location.origin;
+    if (origin === "null" || !origin) {
+      origin = "*";
+    }
+    window.parent.postMessage({
+        type: 'setIframeHeight',
+        iframeId: '${iframeId}',
+        height: height
+    }, origin);
+}
+
+window.addEventListener('load', sendHeightToParent, false);
+window.addEventListener('resize', sendHeightToParent, false);
+
+// there is a delay sometimes. 
+// so we need to send height again after 1 second
+setTimeout(sendHeightToParent, 1000);
+</script>`;
+    // if html contains body tag, then add jscode to body tag of html. else append to html directly
+    if (html.includes("<body>")) {
+      html = html.replace("<body>", `<body>${jsCode}`);
+    } else {
+      html = html + jsCode;
+    }
+
+    return (
+      <iframe
+        srcDoc={html}
+        frameBorder="0"
+        allowFullScreen={false}
+        scrolling="no"
+        id={iframeId}
+        selectable="false"
+        className="w-full"
+      />
+    );
+  };
+
+  const handleOnChange = (event) => {
+    updateAttributes({ code: event.target.value });
+  };
+
+  return (
+    <NodeViewWrapper
+      onClick={() => {
+        setIsEditing(true);
+      }}
+      onBlur={() => {
+        if (blurRemoval) {
+          setIsEditing(false);
+        }
+      }}
+      className="relative"
+    >
+      <NodeViewContent
+        as="btw-embed"
+        code={node.attrs.code}
+        onClick={() => {
+          setIsEditing(true);
+        }}
+      >
+        {isEditing ? (
+          <textarea
+            className="w-full"
+            value={node.attrs.code}
+            onChange={handleOnChange}
+            onBlur={() => {
+              if (blurRemoval) {
+                setIsEditing(false);
+              }
+            }}
+            ref={textareaRef}
+          />
+        ) : (
+          <div>
+            <div className="absolute top-0 left-0 right-0 bottom-0"></div>
+            {getIframe()}
+          </div>
+        )}
+      </NodeViewContent>
+    </NodeViewWrapper>
+  );
+};
 
 const Embed = Node.create({
-  name: "btw-embed", // unique name for the Node
-  group: "block", // belongs to the 'block' group of extensions
-  selectable: true, // so we can select the video
-  draggable: true, // so we can drag the video
-  atom: true, // is a single unit
+  name: "btw-embed",
+  group: "block",
+  selectable: true,
+  draggable: true,
+  atom: true,
 
-  //...
   parseHTML() {
     return [
       {
         tag: "btw-embed",
+        contentElement: "textarea",
       },
     ];
   },
   addAttributes() {
     return {
-      html: {
+      code: {
         default: null,
       },
     };
@@ -33,62 +169,7 @@ const Embed = Node.create({
     ];
   },
   addNodeView() {
-    return ({ editor, node }) => {
-      const embed = document.createElement("btw-embed");
-      const div = document.createElement("div");
-      const iframe = document.createElement("iframe");
-      // convert the html string to a DOM node
-      // &quot; to "
-      let html = (node.attrs.html || "").replace(/&quot;/g, '"');
-
-      // create unique id for iframe
-      const iframeId = "iframe-" + Math.random().toString(36).substr(2, 16);
-
-      const jsCode = `
-      <script>
-    function sendHeightToParent() {
-        var height = document.body.scrollHeight;
-        var origin = window.location.origin;
-        if (origin === "null" || !origin) {
-          origin = "*";
-        }
-        window.parent.postMessage({
-            type: 'setIframeHeight',
-            iframeId: '${iframeId}',
-            height: height
-        }, origin);
-    }
-
-    window.addEventListener('load', sendHeightToParent, false);
-    window.addEventListener('resize', sendHeightToParent, false);
-
-    // there is a delay sometimes. 
-    // so we need to send height again after 1 second
-    setTimeout(sendHeightToParent, 1000);
-</script>`;
-      // if html contains body tag, then add jscode to body tag of html. else append to html directly
-      if (html.includes("<body>")) {
-        html = html.replace("<body>", `<body>${jsCode}`);
-      } else {
-        html = html + jsCode;
-      }
-
-      // insert html into iframe
-      iframe.srcdoc = html;
-      iframe.frameborder = "0";
-      iframe.allowfullscreen = "";
-      iframe.scrolling = "no";
-      iframe.className = "w-full ";
-      iframe.id = iframeId;
-
-      div.appendChild(iframe);
-      div.className = "w-full";
-      embed.appendChild(div);
-      // iframe.src = node.attrs.src;
-      return {
-        dom: embed,
-      };
-    };
+    return ReactNodeViewRenderer((props) => <BtwEmbedNodeView {...props} />);
   },
 });
 
