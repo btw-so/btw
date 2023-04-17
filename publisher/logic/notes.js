e332c30c16d71608ab3b769b4696750f56c2b099
siddhartha gunti <siddhartha.gunti191@gmail.com>
2023-04-17 17:33:21 +0800

    nit bug in sql

--- publisher/logic/notes.js
+++ publisher/logic/notes.js
@@ -4,11 +4,11 @@ async function getAllNotes({ slug, customDomain }) {
   const pool = await db.getTasksDB();
 
   const subquery = customDomain
-    ? `select id from btw.custom_domains where domain = $1`
-    : `select id from btw.users where slug = $1`;
+    ? `select user_id from btw.custom_domains where domain = $1`
+    : `select user_id from btw.users where slug = $1`;
   const { rows } = await pool.query(
     `select slug, published_at, title, tags from btw.notes where publish = TRUE and user_id in (${subquery}) ORDER BY published_at DESC LIMIT 1000`,
-    [customDomain || slug]
+    [slug]
   );
 
   // I will be surprised if someone wrote more than 1000 notes that are published?
@@ -20,12 +20,12 @@ async function getNoteBySlug({ slug, customDomain, noteSlug }) {
   const pool = await db.getTasksDB();
 
   const subquery = customDomain
-    ? `select id from btw.custom_domains where domain = $1`
-    : `select id from btw.users where slug = $1`;
+    ? `select user_id from btw.custom_domains where domain = $1`
+    : `select user_id from btw.users where slug = $1`;
 
   const { rows } = await pool.query(
     `select slug, published_at, title, tags, html from btw.notes where publish = TRUE and user_id in (${subquery}) and slug = $2 LIMIT 1`,
-    [customDomain || slug, noteSlug]
+    [slug, noteSlug]
   );
 
   if (rows.length === 0) {
@@ -39,12 +39,12 @@ async function getUserBySlug({ slug, customDomain }) {
   const pool = await db.getTasksDB();
 
   const subquery = customDomain
-    ? `select id from btw.custom_domains where domain = $1`
-    : `select id from btw.users where slug = $1`;
+    ? `select user_id from btw.custom_domains where domain = $1`
+    : `select user_id from btw.users where slug = $1`;
 
   const { rows } = await pool.query(
     `select name, slug, bio, pic, linkedin, twitter, instagram from btw.users where id in (${subquery}) LIMIT 1`,
-    [customDomain || slug]
+    [slug]
   );
 
   if (rows.length === 0) {
