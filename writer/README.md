84e1435070d97be00da856302594eda19554ad25
siddhartha gunti <siddhartha.gunti191@gmail.com>
2023-10-01 16:27:14 +0530

    Changing default icons + Manifest names

--- writer/README.md
+++ writer/README.md
@@ -1,4 +1,4 @@
-# React-Redux-Saga Boilerplate
+# btw Boilerplate
 
 [![Build Status](https://travis-ci.org/gilbarbara/react-redux-saga-boilerplate.svg?branch=master)](https://travis-ci.org/gilbarbara/react-redux-saga-boilerplate) [![Dependencies](https://david-dm.org/gilbarbara/react-redux-saga-boilerplate.svg)](https://david-dm.org/gilbarbara/react-redux-saga-boilerplate) [![Maintainability](https://api.codeclimate.com/v1/badges/eb66aa0049fa03acbbf3/maintainability)](https://codeclimate.com/github/gilbarbara/react-redux-saga-boilerplate/maintainability) [![Test Coverage](https://api.codeclimate.com/v1/badges/eb66aa0049fa03acbbf3/test_coverage)](https://codeclimate.com/github/gilbarbara/react-redux-saga-boilerplate/test_coverage)
 
--- writer/cypress/integration/basic.js
+++ writer/cypress/integration/basic.js
@@ -1,68 +1,71 @@
 /* eslint-disable jest/expect-expect */
-describe('React-Redux-Saga-Boilerplate', () => {
-  it('should have the correct tile', () => {
-    cy.visit('/');
+describe("btw-Boilerplate", () => {
+  it("should have the correct tile", () => {
+    cy.visit("/");
 
-    cy.title().should('include', 'React Redux Saga Boilerplate');
+    cy.title().should("include", "React Redux Saga Boilerplate");
   });
 
-  it('should have a login button and start the app', () => {
-    cy.findByTestId('Login').should('contain', 'Start').click();
+  it("should have a login button and start the app", () => {
+    cy.findByTestId("Login").should("contain", "Start").click();
   });
 
-  it('should view the private area', () => {
-    cy.findByTestId('Private').should('have.length', 1);
+  it("should view the private area", () => {
+    cy.findByTestId("Private").should("have.length", 1);
   });
 
-  it('should have an alert and dismiss it', () => {
-    cy.findByTestId('Alert').should('have.length', 1).findByRole('button').click();
+  it("should have an alert and dismiss it", () => {
+    cy.findByTestId("Alert")
+      .should("have.length", 1)
+      .findByRole("button")
+      .click();
 
-    cy.findByTestId('Alert').should('not.exist');
+    cy.findByTestId("Alert").should("not.exist");
   });
 
   it('should render the "react" repos', () => {
-    cy.findByTestId('GitHubGrid')
-      .should('have.length', 1)
-      .should('have.attr', 'data-topic', 'react')
-      .find('a')
-      .should('have.length', 30);
+    cy.findByTestId("GitHubGrid")
+      .should("have.length", 1)
+      .should("have.attr", "data-topic", "react")
+      .find("a")
+      .should("have.length", 30);
     cy.wait(2000);
   });
 
-  it('should toggle the selector', () => {
-    cy.findByTestId('GitHubSelector')
-      .find('button:last-child')
-      .should('have.text', 'Redux')
+  it("should toggle the selector", () => {
+    cy.findByTestId("GitHubSelector")
+      .find("button:last-child")
+      .should("have.text", "Redux")
       .click();
   });
 
   it('should render the "redux" repos', () => {
-    cy.findByTestId('GitHubGrid')
-      .should('have.length', 1)
-      .should('have.attr', 'data-topic', 'redux')
-      .find('a')
-      .should('have.length', 30);
+    cy.findByTestId("GitHubGrid")
+      .should("have.length", 1)
+      .should("have.attr", "data-topic", "redux")
+      .find("a")
+      .should("have.length", 30);
 
     cy.wait(2000);
   });
 
-  it('should be able to logout', () => {
-    cy.get('[class^=Logout]').click();
+  it("should be able to logout", () => {
+    cy.get("[class^=Logout]").click();
   });
 
-  it('should have redirected to /', () => {
-    cy.findByTestId('Home').should('have.length', 1);
+  it("should have redirected to /", () => {
+    cy.findByTestId("Home").should("have.length", 1);
   });
 
-  it('should be able to start again', () => {
-    cy.findByTestId('Login').should('contain', 'Start').click();
+  it("should be able to start again", () => {
+    cy.findByTestId("Login").should("contain", "Start").click();
 
-    cy.findByTestId('Private').should('have.length', 1);
+    cy.findByTestId("Private").should("have.length", 1);
   });
 
-  it('should be able to logout again', () => {
-    cy.get('[class^=Logout]').click();
+  it("should be able to logout again", () => {
+    cy.get("[class^=Logout]").click();
 
-    cy.findByTestId('Home').should('have.length', 1);
+    cy.findByTestId("Home").should("have.length", 1);
   });
 });
