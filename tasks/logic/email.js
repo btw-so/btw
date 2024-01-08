1fb28fb6230961c4cd5c9e746e0fba3730272952
siddhartha gunti <siddhartha.gunti191@gmail.com>
2024-01-08 10:54:39 +0530

    Custom domain setup email is working as expected. Removing old email

--- tasks/logic/email.js
+++ tasks/logic/email.js
@@ -90,21 +90,10 @@ async function customDomainSetupEmail({ email, domain }) {
         cc: process.env.ADMIN_EMAIL,
         html: `Hello!<br><br>Thanks for signing up for btw. Super excited to see how your personal blog will shape up!<br><br>Here’s the custom domain you requested: ${domain}<br><br>Custom domains are part of our <a href="https://www.btw.so/pricing">Pro plan (99$/year)</a>. Sharing <a href="https://buy.stripe.com/3csaGkaYB9gR0eI9AA">the Stripe link here</a> for you to complete the purchase.<br><br>Allow us 24 hours post purchase to send you set up instructions.<br><br>Let me know if you have any questions.<br><br>Cheers,<br>${
             (process.env.ADMIN_NAME || "Team btw").split(",")[0]
-        }<br>Team btw`,
+        }`,
         text: `Hello! Thanks for signing up for btw. Super excited to see how your personal blog will shape up! Here’s the custom domain you requested: ${domain} Custom domains are part of our Pro plan (99$/year). Sharing the Stripe link here - https://buy.stripe.com/3csaGkaYB9gR0eI9AA for you to complete the purchase. Allow us 24 hours post purchase to send you set up instructions. Let me know if you have any questions. Cheers, ${
             (process.env.ADMIN_NAME || "Team btw").split(",")[0]
-        } Team btw`,
-    });
-
-    console.log("Message sent: %s", info.messageId);
-
-    // send mail to user that we have received the request
-    info = await transporter.sendMail({
-        from: `"Support" <${process.env.SMTP_FROM}>`, // sender address
-        to: email, // list of receivers
-        subject: "btw - Custom Domain Setup Request", // Subject line
-        html: `<p><b>Hello!</b><br/><br/>We have received your request to setup a custom domain. You will receive further details soon. <br/>Domain: ${domain}<br/></p>Cheers<br/>Team btw`,
-        text: `Hello! We have received your request to setup a custom domain. You will receive further details soon. Domain: ${domain} Cheers, Team btw`,
+        }`,
     });
 
     console.log("Message sent: %s", info.messageId);
