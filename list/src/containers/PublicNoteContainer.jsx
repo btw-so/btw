6c76796ce6c00dff848fbb1587de2bf6c9f1869c
Siddhartha Gunti <siddhartha.gunti191@gmail.com>
2025-05-21 18:15:08 +0530

    Nit css -n

--- list/src/containers/PublicNoteContainer.jsx
+++ list/src/containers/PublicNoteContainer.jsx
@@ -4,7 +4,6 @@ import { useUpdateEffect } from "react-use";
 import { useParams } from "react-router-dom";
 import { selectList } from "../selectors";
 import useTreeChanges from "tree-changes-hook";
-import { motion } from "framer-motion";
 
 import { useAppSelector } from "modules/hooks";
 
@@ -24,15 +23,14 @@ function PublicNoteContainer() {
 
   return (
     <div key="PublicNote" data-testid="PublicNote" className="min-h-full flex flex-col shrink-0 grow">
-        <motion.div
-          className="w-full h-0.5 border-b border-gray-100 h-2 md:h-12 flex"
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ duration: 1.2 }}
+        <div
+          className="w-full h-0.5 border-b border-gray-100 h-6 md:h-12 flex"
         >
-          <div className="container mx-auto px-2 md:px-12 pt-2 md:pt-12 border-solid border-gray-100 border-x max-w-5xl w-full"></div>
-        </motion.div>
-        <div className="container mx-auto px-2 md:px-12 pt-2 md:pt-12 border-solid border-gray-100 border-x max-w-5xl w-full tiptap-editor flex-grow flex-shrink-0">
+          <div className="container mx-6 md:mx-auto px-6 md:px-12 pt-0 md:pt-12 border-solid border-gray-100 border-x max-w-5xl w-full"></div>
+        </div>
+        <div 
+          className="container mx-6 w-auto md:w-full md:mx-auto px-6 md:px-12 pt-6 md:pt-12 border-solid border-gray-100 border-x max-w-5xl tiptap-editor flex-grow flex-shrink-0"
+        >
           <div className="text-3xl font-bold mb-3 leading-tight tracking-tight">
             {list.publicNote?.data?.heading}
           </div>
@@ -45,14 +43,11 @@ function PublicNoteContainer() {
             />
           </div>
         </div>
-        <motion.div
-          className="w-full h-0.5 border-t border-gray-100 h-2 md:h-12 flex"
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ duration: 1.2, delay: 0.2 }}
+        <div
+          className="w-full h-0.5 border-t border-gray-100 h-6 md:h-12 flex"
         >
-          <div className="container mx-auto px-2 md:px-12 pt-2 md:pt-12 border-solid border-gray-100 border-x max-w-5xl w-full"></div>
-        </motion.div>
+          <div className="container mx-6 md:mx-auto px-6 md:px-12 pt-0 md:pt-12 border-solid border-gray-100 border-x max-w-5xl w-full"></div>
+        </div>
     </div>
   );
 }
