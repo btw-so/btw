6fb735abfc38f51838530f24aaa403d7dd44911c
Siddhartha Gunti <siddhartha.gunti191@gmail.com>
2025-11-19 23:33:29 +0800

    nit css

--- locus/src/routes/Dash.jsx
+++ locus/src/routes/Dash.jsx
@@ -160,7 +160,7 @@ function Dash(props) {
               </p>
             </div>
           ) : (
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-3">
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-3">
               {pinnedNotesWithContent.map((node, index) => (
                 <div
                   key={node.id}
